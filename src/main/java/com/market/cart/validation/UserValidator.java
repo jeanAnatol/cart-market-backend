38c082808d604e06339e837d1d3ce68a69f95241
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-06 13:27:44 +0200

    Feat: validator for user update dto

--- src/main/java/com/market/cart/validation/UserValidator.java
+++ src/main/java/com/market/cart/validation/UserValidator.java
@@ -1,9 +1,13 @@
 package com.market.cart.validation;
 
+import com.market.cart.entity.user.User;
 import com.market.cart.entity.user.UserInsertDTO;
 import com.market.cart.entity.user.UserRepository;
+import com.market.cart.entity.user.UserUpdateDTO;
 import com.market.cart.exceptions.custom.CustomInvalidArgumentException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,11 +15,27 @@ import org.springframework.stereotype.Component;
 public class UserValidator {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void validateInsertDTO(UserInsertDTO uInsDTO) {
 
         if(uInsDTO == null) {
             throw new CustomInvalidArgumentException("UserInsertDTO cannot be null.", "userValidator");
+        }
+        if (uInsDTO.username().isEmpty() ||
+        uInsDTO.email().isEmpty() ||
+        uInsDTO.password().isEmpty()) {
+            throw new CustomInvalidArgumentException("\n\nOne of the following values are empty or blank which is forbidden: \n" +uInsDTO.username()+"\n"+uInsDTO.email()+"\n or the password.", "userValidator-validateInsertDTO");
+        }
+    }
+
+    public void validateUpdateDTO(UserUpdateDTO updateDTO, User user) {
+
+        if (updateDTO.newPassword() != null) {
+            if (!passwordEncoder.matches(updateDTO.currentPassword(), user.getPassword())) {
+                throw new BadCredentialsException("Wrong password");
+            }
+            user.setPassword(passwordEncoder.encode(updateDTO.newPassword()));
         }
     }
 }

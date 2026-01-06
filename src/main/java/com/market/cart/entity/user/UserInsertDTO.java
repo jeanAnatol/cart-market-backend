30780a23387590df98aee505b183405756f88237
YannisAnatolitis <jeannanatol@gmail.com>
2026-01-06 13:35:45 +0200

    Fix: remove roleId from UserInsertDTO

--- src/main/java/com/market/cart/entity/user/UserInsertDTO.java
+++ src/main/java/com/market/cart/entity/user/UserInsertDTO.java
@@ -10,19 +10,17 @@ import java.util.Set;
 
 public record UserInsertDTO(
 
-//        @NotEmpty(message = "Username is required.")
+        @NotEmpty(message = "Username is required.")
         String username,
 
-//        @NotEmpty(message = "Password is required.")
+        @NotEmpty(message = "Password is required.")
 //        @Pattern(regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\\d])(?=.*?[!@#$%^&*()-+=]).{8,}$",
 //                message = "Password needs to be 8 characters long and contain one of the following characters:"+
 //                                "0-9, A-Z, a-z, !@#$%^&*()-+=")
         String password,
 
-//        @NotEmpty(message = "Email is required.")
+        @NotEmpty(message = "Email is required.")
 //        @Pattern(regexp = "^(.+)@(.+)$", message = "Email is invalid.")
-        String email,
-
-        Long roleId
+        String email
 ) {
 }
